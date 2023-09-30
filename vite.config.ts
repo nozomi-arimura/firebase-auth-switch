99573574d4440c201eaa02ced06f4050330ec635
arimura-ecn <arimura@ecninc.co.jp>
2023-10-01 06:02:27 +0900

    [fix] サーバー設定

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,11 @@ const manifest = defineManifest({
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
 });
